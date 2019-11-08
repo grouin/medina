d6ba349357d298e16f387bdc72cd7bb5afaa3d17
Cyril Grouin <cyril.grouin@limsi.fr>
2019-11-08 16:41:25 +0100

    Sample files added

--- lanceur.sh
+++ lanceur.sh
@@ -1,32 +1,27 @@
 #!/usr/bin/sh
 
-# On suppose que des corpus d'apprentissage et de test annotés avec
-# BRAT existent dans des répertoires corpus/appr/ et corpus/test/
+# We assume the existence of BRAT annotations in corpus/train/ and
+# corpus/test directories
 
-# Conversion du format BRAT vers un format d'annotations embarquées
-# (fichiers *.tag)
-# - un argument : chemin vers les fichiers *{ann,txt}
-perl zero_alignement.pl corpus/appr/
+# Conversion from BRAT to embedded annotations (files *.tag)
+# - one argument: path to *{ann,txt} files
+perl zero_alignement.pl corpus/train/
 perl zero_alignement.pl corpus/test/
 
-# Production du tabulaire au format BIO pour les CRF de chaîne
-# linéaire
-# - trois arguments : chemin vers les fichiers d'annotations
-#   embarquées, extension de ces fichiers (tag), et nom des tabulaires
-#   produits en sortie
-perl zero_tabulaire.pl corpus/appr/ tag tab_train.zero
+# Production of tabular file using the BIO schema for linear chain CRF
+# - three arguments: path to embedded annotations files, file
+#   extension for those files (tag), and name of output tabular file
+perl zero_tabulaire.pl corpus/train/ tag tab_train.zero
 perl zero_tabulaire.pl corpus/test/ tag tab_test.zero
 
-# Construction du modèle statistique avec Wapiti sur les données
-# d'apprentissage
+# Statistical model building using the Wapiti tool
 wapiti train -a rprop- -1 0.1 -p zero_config.tpl tab_train.zero modele-zero
 
-# Décodage des données de test
+# Model application on test data
 wapiti label -p -m modele-zero tab_test.zero >sortie-zero
 
-# Evaluation des prédictions du modèle (script des campagnes
-# d'évaluation conll)
+# Prediction output evaluation (script from the conll challenge)
 perl conlleval.pl -d '\t' <sortie-zero
 
-# Affichage des faux positifs et faux négatifs pour analyse
+# False positive and false negative analysis
 perl post_differences.pl sortie-zero
