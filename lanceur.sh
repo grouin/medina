74a902cdc189feb528f7e3a5df23fac1dc3b80d5
Cyril Grouin <grouin@limsi.fr>
2019-12-02 08:34:06 +0100

    French legal corpus in its own repository

--- lanceur.sh
+++ lanceur.sh
@@ -5,14 +5,14 @@
 
 # Conversion from BRAT to embedded annotations (files *.tag)
 # - one argument: path to *{ann,txt} files
-perl zero_alignement.pl corpus/train/
-perl zero_alignement.pl corpus/test/
+perl zero_alignement.pl corpus/jorf/train/
+perl zero_alignement.pl corpus/jorf/test/
 
 # Production of tabular file using the BIO schema for linear chain CRF
 # - three arguments: path to embedded annotations files, file
 #   extension for those files (tag), and name of output tabular file
-perl zero_tabulaire.pl corpus/train/ tag tab_train.zero
-perl zero_tabulaire.pl corpus/test/ tag tab_test.zero
+perl zero_tabulaire.pl corpus/jorf/train/ tag tab_train.zero
+perl zero_tabulaire.pl corpus/jorf/test/ tag tab_test.zero
 
 # Statistical model building using the Wapiti tool
 wapiti train -a rprop- -1 0.1 -p zero_config.tpl tab_train.zero modele-zero
