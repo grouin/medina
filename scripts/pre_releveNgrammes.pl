a776de00f215609239a12566aaeb5bd7be75cd58
Cyril Grouin <cyril.grouin@limsi.fr>
2020-01-23 16:54:03 +0100

    Scripts de relevé des fréquences d'utilisation des trigrammes de caractères

---
+++ scripts/pre_releveNgrammes.pl
@@ -0,0 +1,31 @@
+# Relève les tri-grammes de caractères depuis la liste des formes
+# fléchies du français et génère une liste de trigrammes avec leur
+# fréquence d'utilisation dans cette liste
+
+use utf8;
+
+my $fichier="data/forme-lemme-pos.tab";
+my $nbTot=0;
+my %voc=();
+
+open(E,$fichier) or die "Impossible d'ouvrir $fichier\n";
+while (my $l=<E>) {
+    chomp $l;
+    $l=lc($l);
+    my ($token,$lemme,$pos)=split(/\t/,$l);
+    for (my $i=0;$i<length($token)-2;$i++) {
+	my $ngr=substr($token,$i,3);
+	if ($ngr=~/^\p{L}{3}$/) {
+	    #print "$ngr\t";
+	    $nbTot++;
+	    $voc{$ngr}++;
+	}
+    }
+}
+close(E);
+
+open(S,'>:utf8',"data/liste_ngrammes.txt");
+foreach my $ngr (sort keys %voc) {
+    print S "$ngr\t",sprintf("%.6f",$voc{$ngr}/$nbTot)*100,"\n";
+}
+close(S);
