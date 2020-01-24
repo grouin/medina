47f0475d636fd2b226eab69269be7c84053fe6fa
Cyril Grouin <grouin@limsi.fr>
2020-01-24 19:29:45 +0100

    Script de préparation du dictionnaire des formes fléchies du français

--- scripts/pre_creeDictionnaire.bash
+++ scripts/pre_creeDictionnaire.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-for i in {a..z}; do wget http://abu.cnam.fr/cgi-bin/donner-dico-uncompress?liste_$i -O data/liste-$i; done
-for i in $(ls data/liste*); do cat $i | awk "/\t/" | sort | uniq >>data/forme-lemme-pos.tab; done
+for i in {a..z}; do wget http://abu.cnam.fr/cgi-bin/donner-dico-uncompress?liste_$i -O scripts/data/liste-$i; done
+for i in $(ls scripts/data/liste*); do cat $i | awk "/\t/" | sort | uniq >>scripts/data/forme-lemme-pos.tab; done
