428c90bce1a2ce4ff8ed1a25f0e510e08fa223fe
Cyril Grouin <grouin@limsi.fr>
2020-02-07 20:01:24 +0100

    Scripts de traitement des corpus
    - suppression des sauts de lignes excessifs dans la réduction du sur-apprentissage

--- scripts/zero_supprimeO.pl
+++ scripts/zero_supprimeO.pl
@@ -22,6 +22,7 @@ my $n=$ARGV[1]; if (!$ARGV[1]) { $n=6; }
 my %lignes;
 my $i=1000000;
 my %deja=();
+my $contenu="";
 
 
 ###
@@ -33,9 +34,9 @@ my %deja=();
 
 open(E,$fichier);
 while (my $ligne=<E>) {
-    chomp $ligne;
-    $lignes{$i}=$ligne;
-    $i++;
+  chomp $ligne;
+  $lignes{$i}=$ligne;
+  $i++;
 }
 close(E);
 
@@ -43,62 +44,54 @@ close(E);
 # et qui suivent les lignes annotées
 
 foreach $i (sort keys %lignes) {
-    my @cols=split(/\t/,$lignes{$i});
-    my $flag=0;
+  my @cols=split(/\t/,$lignes{$i});
+  my $flag=0;
 
-    # Si la ligne porte une annotation, on affiche la ligne
-    if ($cols[$#cols]=~/^[BIWEMH]/) {
-	print $lignes{$i},"\n" if (!exists $deja{$i});
-	$deja{$i}++;
+  # Si la ligne porte une annotation, on affiche la ligne
+  if ($cols[$#cols]=~/^[BIWEMH]/) {
+    $contenu.="$lignes{$i}\n" if (!exists $deja{$i});
+    $deja{$i}++;
+  }
+
+  # Sinon, on vérifie que dans les /n/ lignes qui précèdent ou qui
+  # suivent, on rencontre une ligne porteuse d'annotation. Dans ce
+  # cas, on affiche la ligne courante (c'est alors une ligne O qui
+  # correspond à un contexte gauche ou droit d'une ligne annotée)
+  elsif ($cols[$#cols]=~/^O/) {
+
+    # On recherche une ligne annotée dans les /n/ lignes qui précèdent
+    for (my $j=0;$j<=$n;$j++) {
+      my $somme=$i-$j;
+      my @cols2=split(/\t/,$lignes{$somme});
+      if ($cols2[$#cols2]=~/^[BIWEMH]/) { $flag=$somme; }
+    }
+    if ($flag>0) {
+      $contenu.="$lignes{$i}\n" if (!exists $deja{$i});
+      $deja{$i}++;
+      $flag=0;
     }
 
-    # Sinon, on vérifie que dans les /n/ lignes qui précèdent ou qui
-    # suivent, on rencontre une ligne porteuse d'annotation ; Si c'est
-    # le cas, on affiche la ligne courante (c'est alors une ligne O
-    # qui correspond à un contexte gauche ou droit d'une ligne
-    # annotée)
-    elsif ($cols[$#cols]=~/^O/) {
-
-	# On recherche une ligne annotée dans les /n/ lignes qui
-	# précèdent
-	for (my $j=0;$j<=$n;$j++) {
-	    my $somme=$i-$j;
-	    my @cols2=split(/\t/,$lignes{$somme});
-	    if ($cols2[$#cols2]=~/^[BIWEMH]/) {
-		$flag=$somme;
-	    }
-	}
-	if ($flag>0) {
-	    print $lignes{$i},"\n" if (!exists $deja{$i});
-	    $deja{$i}++;
-	    $flag=0;
-	}
-
-	# On recherche une ligne annotée dans les /n/ lignes qui
-	# suivent
-	for (my $j=0;$j<=$n;$j++) {
-	    my $somme=$i+$j;
-	    my @cols2=split(/\t/,$lignes{$somme});
-	    if ($cols2[$#cols2]=~/^[BIWEMH]/) {
-		$flag=$somme;
-	    }
-	}
-	if ($flag>0) {
-	    print $lignes{$i},"\n" if (!exists $deja{$i});
-	    $deja{$i}++;
-	    $flag=0;
-	}
-
+    # On recherche une ligne annotée dans les /n/ lignes qui suivent
+    for (my $j=0;$j<=$n;$j++) {
+      my $somme=$i+$j;
+      my @cols2=split(/\t/,$lignes{$somme});
+      if ($cols2[$#cols2]=~/^[BIWEMH]/) { $flag=$somme; }
+    }
+    if ($flag>0) {
+      $contenu.="$lignes{$i}\n" if (!exists $deja{$i});
+      $deja{$i}++;
+      $flag=0;
     }
 
-    # Si on est sur une ligne O et que dans les /n/ lignes qui
-    # précèdent ou qui suivent on ne trouve pas de ligne portant une
-    # annotation, on n'affiche rien, hormis un saut de ligne pour ne
-    # pas générer une seule séquence
-    else {
-	if ($flag==0) {
-	    print "\n";
-	}
-    }
+  }
+
+  # Si on est sur une ligne O et que dans les /n/ lignes qui précèdent
+  # ou qui suivent on ne trouve pas de ligne portant une annotation,
+  # on n'affiche rien, hormis un saut de ligne pour ne pas générer une
+  # seule séquence
+  else { if ($flag==0) { $contenu.="\n"; } }
 
 }
+
+$contenu=~s/\n{2,}/\n\n/g;
+print $contenu;
