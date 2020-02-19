ba195afe9d65cd38aee523fbceea51aa92645ff6
Cyril Grouin <cyril.grouin@limsi.fr>
2020-02-19 17:50:42 +0100

    Scripts de traitement de corpus
    - affichage faux positifs/faux négatifs : possibilité de préciser en 2ème argument
      si on souhaite restreindre aux faux positifs (fp), aux faux négatifs (fn), ou
      tout afficher (fpfn ou ne rien mettre)

--- scripts/post_differences.pl
+++ scripts/post_differences.pl
@@ -5,13 +5,14 @@
 # suivante sans erreur) à partir d'un fichier de prédictions au format
 # tabulaire avec schéma d'annotation BIO
 
-# Usage : perl differences.pl fichier.tab
+# Usage : perl differences.pl fichier.tab fp/fn/fpfn
 
 # Auteur : Cyril Grouin, novembre 2019.
 
 use strict;
 
 my $fichier=$ARGV[0];
+my $type=$ARGV[1]; if (!$ARGV[1]) { $type="fpfn"; }
 my @lignes=();
 my @fp=();
 my @fn=();
@@ -27,13 +28,13 @@ my $i=0;
 foreach my $ligne (@lignes) {
     my @cols=split(/\t/,$ligne);
     # Faux positifs
-    if (($cols[$#cols]!~/^O/) && ($cols[$#cols-1]=~/^O/)) {
+    if (($cols[$#cols]!~/^O/) && ($cols[$#cols-1]=~/^O/) && $type=~/fp/) {
 	my $j=$i-1; $fp[$j]="$j\t$lignes[$j]\n" if ($fp[$j] ne "");
 	$fp[$i]="$i\t$ligne\n";
 	my $k=$i+1; $fp[$k]="$k\t$lignes[$k]\n" if ($fp[$k] ne "");
     }
     # Faux négatifs
-    if (($cols[$#cols]=~/^O/) && ($cols[$#cols-1]!~/^O/)) {
+    if (($cols[$#cols]=~/^O/) && ($cols[$#cols-1]!~/^O/) && $type=~/fn/) {
 	my $j=$i-1; $fn[$j]="$j\t$lignes[$j]\n" if ($fn[$j] ne "");
 	$fn[$i]="$i\t$ligne\n";
 	my $k=$i+1; $fn[$k]="$k\t$lignes[$k]\n" if ($fn[$k] ne "");
